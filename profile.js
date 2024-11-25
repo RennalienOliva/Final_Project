b06630480c47790b8071e4d52f4c25e6aeb7226c
RennalienOliva <rennalienoliva9@gmail.com>
2024-11-25 11:26:24 +0800

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,15 @@
+function on() {
+
+    document.getElementById("profile").style.display = "block";
+
+  }
+
+
+function off(){
+
+  document.getElementById("profile").style.display = "none";
+
+}
+
+ 
+
